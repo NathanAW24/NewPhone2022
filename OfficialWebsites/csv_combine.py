10a38502156af49a30b672c9e7a6aa91231865c6
NathanAW24 <nathan.aldrich.w@gmail.com>
2022-01-27 18:52:41 +0800

    push csv_combine function

--- OfficialWebsites/csv_combine.py
+++ OfficialWebsites/csv_combine.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-
+# this code is used to combine the three different csv obtained from scraping
 df = pd.concat(
     map(pd.read_csv, ['appleRaw.csv', 'asusRaw.csv', 'sonyRaw.csv']), ignore_index=True)
 df.to_csv('OfficialWeb_Phones.csv', index=False)
